1774fd51e15e5627acbb4e1a989ae5399611f4eb
SAI REDDY SHYAMAKURA <saireddy.shyamakura@gmail.com>
2024-03-22 16:15:15 +0530

    Create classes.dart

---
+++ classes.dart
@@ -0,0 +1,42 @@
+void main() {
+//   Cars  build = Cars();
+  
+//   print("before : ${build.shape}");
+//   build.shape = "suv";
+//   print("after : ${build.shape}");
+
+ }
+
+class Cars {
+
+  // shapes = roadster, sedan , suv , cupoe, xuv
+
+  String shape = " sedan"; // sedan default
+   
+  int engine(){
+    // fuel = petrol , desiel, eletrical , hybrid, hydrogen
+    String fuel = "petrol" ;
+    // tires = 19, 18,21,17
+    return 1;
+    
+  }
+  
+  // when function used sepeartely is called fucntion when used in class is called method
+  void interior(){
+    // dashbood = carbonFiber, wood, hardPlastic, softPlastic
+    // seats = heating, ventilated , leather, wool
+    // speakers = jbl, harmonCardon, bang&olsen
+    
+    }
+    void touches(){
+      // colors = metal, gery, blue,custom;
+  }
+  
+  int overall(){
+    bool engine  = false;
+    bool interior = false;
+    bool touches = false;
+      return 0;
+  }
+
+}
